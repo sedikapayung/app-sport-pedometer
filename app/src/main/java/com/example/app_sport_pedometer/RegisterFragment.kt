0304abc3ed0f1f4006ea0825657754cbad0188c9
Sedika <sedikasipayung5@gmail.com>
2025-03-21 15:01:56 +0700

    created register UI and logic

---
+++ app/src/main/java/com/example/app_sport_pedometer/RegisterFragment.kt
@@ -0,0 +1,18 @@
+package com.example.app_sport_pedometer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RegisterFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}
