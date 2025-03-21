3925f7a0e3268483bb028fda593199452d507fdb
Sedika <sedikasipayung5@gmail.com>
2025-03-21 15:58:32 +0700

    Solved bug

--- app/src/main/java/com/example/app_sport_pedometer/LoginFragment.kt
+++ app/src/main/java/com/example/app_sport_pedometer/LoginFragment.kt
@@ -6,14 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+
 class LoginFragment : Fragment() {
-
-
-    class LoginFragment : Fragment() {
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-        ): View? {
-            return inflater.inflate(R.layout.fragment_login, container, false)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 }
--- app/src/main/java/com/example/app_sport_pedometer/MainActivity.kt
+++ app/src/main/java/com/example/app_sport_pedometer/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app_sport_pedometer.adapter.ViewPagerAdapter
+import com.example.app_sport_pedometer.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        var tablayout = binding.tabLayout
+        var viewPager = binding.viewPager
+
+        viewPager.adapter = ViewPagerAdapter(this)
+
+        TabLayoutMediator(tablayout, viewPager){
+            tab,position ->
+            tab.text = if (position == 0 ) "Login" else "Register"
+        }.attach()
+
     }
 }
