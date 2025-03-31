e061316efa9303b054e4262cbb7de3e31035fd30
Sedika <sedikasipayung5@gmail.com>
2025-03-31 11:17:50 +0700

    Add intent for navigate from login to main buttom navigation

--- app/src/main/java/com/example/app_sport_pedometer/LoginFragment.kt
+++ app/src/main/java/com/example/app_sport_pedometer/LoginFragment.kt
@@ -1,16 +1,31 @@
 package com.example.app_sport_pedometer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.app_sport_pedometer.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+    private  var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnLogin.setOnClickListener{
+            val intent = Intent(requireContext(),MainActivity2::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
     }
 }
